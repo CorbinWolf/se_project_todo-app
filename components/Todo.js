50d09942ed9115b0b6e391c8528d8b3217a1aae5
Corbin Wolf <corbinwolf300@gmail.com>
2025-04-20 23:48:43 -0700

    feat(TodoCounter.js): add todo completion visualizer

--- components/Todo.js
+++ components/Todo.js
@@ -1,7 +1,12 @@
 class Todo {
-  constructor(data, selector) {
-    this._data = data;
+  constructor(data, selector, handleCheck, handleDelete) {
+    this._name = data.name;
+    this._date = data.date;
+    this._id = data.id;
+    this._completed = data.completed;
     this._todoTemplate = document.querySelector(selector);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
   }
 
   _setEventListeners() {
@@ -9,11 +14,13 @@ class Todo {
       this._todoElement.querySelector(".todo__delete-btn");
 
     todoDeleteButton.addEventListener("click", () => {
+      this._handleDelete(this._completed);
       this._todoElement.remove();
     });
 
     this._todoCheckboxElement.addEventListener("change", () => {
-      this._data.completed = !this._data.completed;
+      this._toggleCompletion();
+      this._handleCheck(this._completed);
     });
   }
 
@@ -22,14 +29,18 @@ class Todo {
       this._todoElement.querySelector(".todo__completed");
     this._todoLabel = this._todoElement.querySelector(".todo__label");
 
-    this._todoCheckboxElement.checked = this._data.completed;
-    this._todoCheckboxElement.id = `todo-${this._data.id}`;
-    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+    this._todoCheckboxElement.checked = this._completed;
+    this._todoCheckboxElement.id = `todo-${this._id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._id}`);
+  }
+
+  _toggleCompletion() {
+    this._completed = !this._completed;
   }
 
   _getDate() {
     const todoDate = this._todoElement.querySelector(".todo__date");
-    const dueDate = new Date(this._data.date);
+    const dueDate = new Date(this._date);
 
     if (!isNaN(dueDate)) {
       todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
@@ -46,7 +57,7 @@ class Todo {
       .cloneNode(true);
     const todoNameElement = this._todoElement.querySelector(".todo__name");
 
-    todoNameElement.textContent = this._data.name;
+    todoNameElement.textContent = this._name;
 
     this._getDate();
     this._generateCheckboxElement();
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,10 @@ import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import TodoCounter from "../components/TodoCounter.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopupElement = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
-const addTodoCloseButton = addTodoPopupElement.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
 
 const section = new Section({
@@ -23,15 +22,37 @@ const section = new Section({
 
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
-  handleFormSubmit: () => {},
+  handleFormSubmit: (inputValues) => {
+    const name = inputValues.name;
+    const dateInput = inputValues.date;
+
+    const date = new Date(dateInput);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+    const id = uuidv4();
+    const values = { name, date, id };
+    section.addItem(values);
+    todoValidator.resetValidation();
+    addTodoPopup.close();
+    todoCounter.updateTotal(true);
+  },
 });
 
-function closeModal(modal) {
-  modal.classList.remove("popup_visible");
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
+
+function handleCheck(completed) {
+  todoCounter.updateCompleted(completed);
+}
+
+function handleDelete(completed) {
+  if (completed) {
+    todoCounter.updateCompleted(false);
+  }
+  todoCounter.updateTotal(false);
 }
 
 function generateTodo(data) {
-  const todo = new Todo(data, "#todo-template");
+  const todo = new Todo(data, "#todo-template", handleCheck, handleDelete);
   const todoElement = todo.getView();
   return todoElement;
 }
@@ -40,24 +61,7 @@ addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
 
-addTodoCloseButton.addEventListener("click", () => {
-  closeModal(addTodoPopupElement);
-});
-
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  const date = new Date(dateInput);
-  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
-  const id = uuidv4();
-  const values = { name, date, id };
-  section.addItem(values);
-  todoValidator.resetValidation();
-  closeModal(addTodoPopupElement);
-});
+addTodoPopup.setEventListeners();
 
 section.renderItems();
 
